ee179901068111e324321f6835958097ea8d8bfd
yehongHan <yehong.han@ly.com>
2019-04-19 21:58:46 +0800

    聊天机器人DTO参数接收类封装

---
+++ xydl-life/src/main/java/com/njit/xydl/life/robot/controller/dto/ChatDTO.java
@@ -0,0 +1,12 @@
+package com.njit.xydl.life.robot.controller.dto;
+
+import lombok.Data;
+
+/**
+ * @author yehong.han
+ * @date 2019/4/19
+ */
+@Data
+public class ChatDTO {
+	private String msg;
+}
